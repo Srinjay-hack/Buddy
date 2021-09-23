7bac61d34d1f4109c039987dd684ca37e4a6cf8d
Srinjay-hack <srinjaymondal@gmail.com>
2021-09-24 01:54:47 +0530

    geroku

--- buddy/settings.py
+++ buddy/settings.py
@@ -114,4 +114,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media") 
 MEDIA_URL= "/media/"
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
